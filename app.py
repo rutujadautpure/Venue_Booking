0a45c6e06464f3d3e251b78d8fcc7026d7b4d16b
khedkarsayali <sayalikhedkar6@gmail.com>
2024-04-09 19:56:47 +0530

    database

--- app.py
+++ app.py
@@ -1,1 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/datahub'  # MySQL database URL
+db = SQLAlchemy(app)
+
+
+class Event(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    event_name = db.Column(db.String(500))
+    manager_name = db.Column(db.String(500))
+    club_name = db.Column(db.String(100), nullable=True)
+    event_date = db.Column(db.String(100))
+    start_time = db.Column(db.Time)
+    end_time = db.Column(db.Time)
+    hall_name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    ph_num = db.Column(db.BigInteger)
+    department = db.Column(db.String(100), nullable=True)
+    request = db.Column(db.DateTime)
+    status = db.Column(db.String(255))
+
+
+class User(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+    contactNo = db.Column(db.String(20), nullable=True)
+    clubname = db.Column(db.String(100), nullable=True)
+    department = db.Column(db.String(100), nullable=True)
+
+
+class Venue(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    capacity = db.Column(db.Integer)
+    features = db.Column(db.String(255))
